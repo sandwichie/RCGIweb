1d7a703652eb08e931e22390870a95968bb564d1
sandwichie <124646039+sandwichie@users.noreply.github.com>
2025-04-04 18:16:07 +0800

    Update admin_settings.php

--- admin_settings.php
+++ admin_settings.php
@@ -66,7 +66,7 @@ $conn->close();
   <title>RCGI | SETTINGS</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <link rel="icon" href="pics/rcgiph_logo.jpg" type="image/x-icon">
+  <link rel="icon" href="pics/rcgiph_logo.png" type="image/x-icon">
   <style>
     body {
       font-family: 'Inter', sans-serif;
@@ -311,7 +311,7 @@ $conn->close();
       </a>
     </div>
     <div class="w-100 text-center pb-3">
-      <img src="pics/rcgiph_logo.jpg" class="img-fluid" alt="Logo" style="max-width: 50%; height: auto;">
+      <img src="pics/rcgiph_logo.png" class="img-fluid" alt="Logo" style="max-width: 50%; height: auto;">
     </div>
   </div>
 
